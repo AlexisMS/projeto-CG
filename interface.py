05fd54b768a38b9605457cddd57fc0f9d27d0a61
Alexis Mendes Sequeira <alexisora@gmail.com>
2024-09-18 01:10:41 -0300

    preparando para rotate point

--- interface.py
+++ interface.py
@@ -531,7 +531,7 @@ class MainWindow(QMainWindow):
         else:
             obj = self.selected_object()
             angle = float(self.angle_entry.text())
-            obj.transform_rotation(angle)
+            obj.transform_rotation(angle,obj.get_center())
             obj.apply_transform()
             obj.reset_transform()
             self.redraw_objects()
