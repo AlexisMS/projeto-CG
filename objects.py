e13710c37042cfad2eeaf1efce7823308a3c82d2
Alexis Mendes Sequeira <alexisora@gmail.com>
2024-09-18 01:50:48 -0300

    rotação funcionando?? ou não???

--- objects.py
+++ objects.py
@@ -112,36 +112,9 @@ class WireFrame():
     
     def transform_rotation(self,angle, pivot: Point):
         #pivot = self.get_center()
-        if self.center.get_x()>0 and self.center.get_y()>0:
-            self.transform_translate(-pivot.get_x(), -pivot.get_y())
-            self.transform_basic_rotation(angle)
-            self.transform_translate(pivot.get_x(), pivot.get_y())
-        elif self.center.get_x()>0 and self.center.get_y()<0:
-            self.transform_translate(-pivot.get_x(), pivot.get_y())
-            self.transform_basic_rotation(angle)
-            self.transform_translate(pivot.get_x(), -pivot.get_y())
-        elif self.center.get_x()<0 and self.center.get_y()<0:
-            self.transform_translate(pivot.get_x(), pivot.get_y())
-            self.transform_basic_rotation(angle)
-            self.transform_translate(-pivot.get_x(), -pivot.get_y())
-        elif self.center.get_x()==0 and self.center.get_y()>0:
-            self.transform_translate( 0, -pivot.get_y())
-            self.transform_basic_rotation(angle)
-            self.transform_translate(0, pivot.get_y())
-        elif self.center.get_x()==0 and self.center.get_y()<0:
-            self.transform_translate(0, pivot.get_y())
-            self.transform_basic_rotation(angle)
-            self.transform_translate(0, -pivot.get_y())
-        elif self.center.get_x()>0 and self.center.get_y()==0:
-            self.transform_translate(-pivot.get_x(), 0)
-            self.transform_basic_rotation(angle)
-            self.transform_translate(pivot.get_x(), 0)
-        elif self.center.get_x()<0 and self.center.get_y()==0:
-            self.transform_translate(pivot.get_x(), 0)
-            self.transform_basic_rotation(angle)
-            self.transform_translate(-pivot.get_x(), 0)
-        elif self.center.get_x()==0 and self.center.get_y()==0:
-            self.transform_basic_rotation(angle)
+        self.transform_translate(-pivot.get_x(), -pivot.get_y())
+        self.transform_basic_rotation(angle)
+        self.transform_translate(pivot.get_x(), pivot.get_y())
         
 
     def transform_translate(self, dx, dy):
